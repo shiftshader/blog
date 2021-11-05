6ff147524604ce4b807667da788db87c0c61769c
Nikola Stankovic <nikolastankovic1990@gmail.com>
2021-11-05 02:20:48 +0100

    cetvrti zadatak

---
+++ dbBlog.php
@@ -0,0 +1,18 @@
+<?php
+    $servername = "127.0.0.1";
+    $username = "john";
+    $password = "";
+    $dbname = "blog";
+
+    try {
+        $connection = new PDO(
+            "mysql:host=$servername;dbname=$dbname", $username, $password
+        );
+       
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
